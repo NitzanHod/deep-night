7f718c0045c9d0fa0a2df58dd70870300e63bef6
NitzanHod <hodosnitzan@gmail.com>
2020-02-18 15:44:04 +0200

    Experiment configuration file generator

--- utils/cfg_utils.py
+++ utils/cfg_utils.py
@@ -3,6 +3,8 @@ from sacred import Experiment, Ingredient
 import yaml
 import os
 from shutil import copy2
+
+
 def _to_camel_case(in_str):
     components = in_str.split('_')
     return ''.join(x.title() for x in components)
@@ -16,7 +18,7 @@ class ExperimentManager:
 
     def setup_cfg(self, path):
         if path in ['sid', 'deepisp', 'test']:
-            path = f'cfg/full_cfg_{path}.yml'
+            path = f'cfg/train_sid_{path}.yml'
 
         with open(path, 'r') as f:
             return path, yaml.safe_load(f)
@@ -40,6 +42,19 @@ class ExperimentManager:
         _, filename = os.path.split(self.config_path)
         cfg_store_path = os.path.join(dirname, filename)
         copy2(self.config_path, cfg_store_path)  # copy file
+
+    @staticmethod
+    def generate_cfg(params : dict , name : str):
+        path = 'cfg/train_sid.yml'
+        result_path = f'cfg/train_sid_{name}.yml'
+        with open(path, 'r') as f:
+            dic = yaml.safe_load(f)
+        for cfg_section in params.keys():
+            for k,v in params[cfg_section].items():
+                if k in dic[cfg_section].keys():
+                    dic[cfg_section][k] = v
+        with open(result_path, 'w') as f:
+            yaml.safe_dump(dic, f)
 
 class Component:
     def __init__(self, component_name, config):
@@ -67,3 +82,9 @@ class Component:
 
     def __repr__(self):
         return str(self)
+
+
+if __name__ == '__main__':
+    ExperimentManager.generate_cfg({'train_cfg':{'model':'cfg/model_cfg/dropout_u_net.cfg'}}, 'dropout')
+    ExperimentManager.generate_cfg({'train_cfg':{'weight_decay': 0.1}}, 'reg')
+    ExperimentManager.generate_cfg({'train_cfg':{'model':'cfg/model_cfg/dropout_u_net.cfg', 'weight_decay': 0.1}}, 'dropout_reg')
